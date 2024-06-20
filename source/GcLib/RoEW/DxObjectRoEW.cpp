975136cdab65321cf310c019f26f2057991d9cd1
WishMakers0 <kirbydan830@gmail.com>
2024-06-19 22:37:18 -0400

    a bit more progress on menu objects...

--- source/GcLib/RoEW/DxObjectRoEW.cpp
+++ source/GcLib/RoEW/DxObjectRoEW.cpp
@@ -2,14 +2,25 @@
 
 #include "../directx/DxObject.hpp"
 #include "../directx/DxScript.hpp"
+#include "../directx/DirectInput.hpp"
 
 using namespace directx;
 
 DxMenuObject::DxMenuObject() {
 	timer = 0;
+	// set up buttonTimer correctly
+	buttonTimer.insert({EDirectInput::KEY_LEFT, 0});
+	buttonTimer.insert({EDirectInput::KEY_RIGHT, 0});
+	buttonTimer.insert({EDirectInput::KEY_UP, 0});
+	buttonTimer.insert({EDirectInput::KEY_DOWN, 0});
+	buttonTimer.insert({EDirectInput::KEY_SHOT, 0});
+	buttonTimer.insert({EDirectInput::KEY_BOMB, 0});
+	buttonTimer.insert({EDirectInput::KEY_USER1, 0});
+	buttonTimer.insert({EDirectInput::KEY_USER2, 0});
 }
 
 void DxMenuObject::Activate() {
+	//Only activate your menu object *after* you set up all the important parts!
 }
 
 void DxMenuObject::Work() {
--- source/GcLib/RoEW/DxObjectRoEW.hpp
+++ source/GcLib/RoEW/DxObjectRoEW.hpp
@@ -3,6 +3,7 @@
 #include "../directx/DxObject.hpp"
 #include "../directx/DxScript.hpp"
 #include "../directx/DirectInput.hpp"
+#include "../../TouhouDanmakufu/Common/DnhGcLibImpl.hpp"
 
 namespace directx {
 
@@ -20,7 +21,7 @@ namespace directx {
 			//gstd::value idObjectValue_;
 			// Rest in peace, the Jank-o-tron 9000.  We hardly knew ye.
 
-			struct flags {
+			struct flag_bitfield {
 				bool disable		: 1;
 				bool error			: 1;
 				bool close			: 1;
@@ -32,6 +33,7 @@ namespace directx {
 			};
 			// This syntax for flag storage is actually absurdly helpful.
 			// Vastly prefer it to normal bitfields with bitwise arithmetic...
+			flag_bitfield flags;
 			int timer;
 			enum t_option {
 				TYPE_INVALID = 0,
@@ -43,12 +45,14 @@ namespace directx {
 			};
 			gstd::ref_count_weak_ptr<DxScriptObjectBase, false> parent;
 			unsigned int optionIndex;
-			std::vector<gstd::ref_count_weak_ptr<DxScriptObjectBase>> relatedIDs;
+			unsigned int maxIndex;
+			std::vector<gstd::ref_count_weak_ptr<DxScriptObjectBase, false>> relatedIDs;
 			std::map<unsigned int, unsigned int> optionIndexX;
-			std::map<unsigned int, int> maxIndex;
+			std::map<unsigned int, int> maxIndexX;
 			std::map<unsigned int, float> sliderValue;
 			std::map<unsigned int, int> sliderMin;
 			std::map<unsigned int, int> sliderMax;
+			std::vector<t_option> optionType;
 			std::map<int16_t, int> buttonTimer;
 			int16_t lastKey;
 
@@ -56,18 +60,47 @@ namespace directx {
 			DxMenuObject();
 			virtual void Activate();
 			virtual void Work();
+
+			int GetParent() { return parent->GetObjectID(); }
+			bool GetDisabled() { return flags.disable; }
+			int GetRelatedObject(int index) { return relatedIDs[index] != nullptr ? relatedIDs[index]->GetObjectID() : DxScript::ID_INVALID; }
+			int GetOptionIndex() { return optionIndex; }
+			int GetOptionIndexX(int index) { return optionIndexX[index]; }
+			int GetMaxIndex() { return maxIndex; }
+			int GetMaxIndexX(int index) { return maxIndexX[index]; }
+			float GetSliderValue(int index) { return sliderValue[index]; }
+			int GetSliderMin(int index) { return sliderMin[index]; }
+			int GetSliderMax(int index) { return sliderMax[index]; }
+			int GetOptionType(int index) { return (int)optionType[index]; }
+
 			void SetParent(DxScriptObjectBase* _p) { parent = _p; }
+			void AddRelatedObject(DxScriptObjectBase* _obj) { 
+				gstd::ref_count_weak_ptr<DxScriptObjectBase, false> _o; 
+				_o = _obj; 
+				relatedIDs.push_back(_o); 
+			}
+			void SetOptionIndex(unsigned int _arg) { optionIndex = _arg; }
+			void SetMaxIndex(unsigned int _arg) { maxIndex = _arg; }
+			void SetOptionIndexX(unsigned int index, unsigned int _a2) { optionIndexX[index] = _a2; }
+			void SetMaxIndexX(unsigned int index, unsigned int _a2) { maxIndexX[index] = _a2; }
+			void SetSliderValue(unsigned int index, float f) { sliderValue[index] = f; }
+			void SetSliderMax(unsigned int index, int val) { sliderMax[index] = val; }
+			void SetSliderMin(unsigned int index, int val) { sliderMin[index] = val; }
+
 	};
 
 	class DxMenuObjectManager : public DxScriptObjectBase {
 		friend DxScript;
 
 	protected:
+		std::vector<int> menuIDs;
 		std::map<int, gstd::value> menuResult;
 
 	public:
 		DxMenuObjectManager();
 		void ForceCloseAllMenus();
+		gstd::value GetReturnValue(int oid) { return menuResult[oid]; }
+		void SetReturnValue(int oid, gstd::value val) { menuResult[oid] = val; }
 	};
 
 }
